0ae4724db580b70d387a8c22b6c1509abbc39bcb
Egor Merkushev <gorkae@rambler.ru>
2013-08-13 12:35:37 +0400

    Initial Commit

---
+++ ColorChooser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorChooser
+//
+//  Created by Sound Blaster on 13.08.13.
+//  Copyright (c) 2013 Sound Blaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorChooser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ColorChooser
+//
+//  Created by Sound Blaster on 13.08.13.
+//  Copyright (c) 2013 Sound Blaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

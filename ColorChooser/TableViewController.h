1f611b5042d234b914c62685978e8a17271b2bd6
Egor Merkushev <gorkae@rambler.ru>
2013-08-13 13:47:40 +0400

    Done

---
+++ ColorChooser/TableViewController.h
@@ -0,0 +1,37 @@
+//
+//  TableViewController.h
+//  ColorChooser
+//
+//  Created by Sound Blaster on 13.08.13.
+//  Copyright (c) 2013 Sound Blaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController {
+UIColor *_color;
+}
+
+@property (weak, nonatomic) IBOutlet UISlider *rSlider;
+@property (weak, nonatomic) IBOutlet UISlider *gSlider;
+@property (weak, nonatomic) IBOutlet UISlider *bSlider;
+@property (weak, nonatomic) IBOutlet UILabel *rLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bLabel;
+@property (weak, nonatomic) IBOutlet UIView *controlView;
+
+@property (weak, nonatomic) IBOutlet UISlider *rBgSlider;
+@property (weak, nonatomic) IBOutlet UISlider *gBgSlider;
+@property (weak, nonatomic) IBOutlet UISlider *bBgSlider;
+@property (weak, nonatomic) IBOutlet UILabel *rBgLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gBgLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bBgLabel;
+
+@property (weak, nonatomic) IBOutlet UISwitch *translucentSwitcher;
+@property (weak, nonatomic) IBOutlet UISwitch *blackNavBarSwitcher;
+
+- (IBAction)changeValue:(id)sender;
+- (IBAction)bgChangeValue:(id)sender;
+- (IBAction)switchChanged:(id)sender;
+
+@end
---
+++ ColorChooser/TableViewController.m
@@ -0,0 +1,116 @@
+//
+//  TableViewController.m
+//  ColorChooser
+//
+//  Created by Sound Blaster on 13.08.13.
+//  Copyright (c) 2013 Sound Blaster. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)configureView {
+    // Update the user interface for the detail item.
+    [self changeValue:nil];
+    [self bgChangeValue:nil];
+    [self switchChanged:nil];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self configureView];
+}
+
+- (IBAction)changeValue:(id)sender {
+    UILabel *targetLabel;
+    NSString *formatString = @"%1.2f";
+    if (sender == self.rSlider) {
+        targetLabel = self.rLabel;
+        formatString = @"R: %1.2f";
+    }
+    
+    if (sender == self.gSlider) {
+        targetLabel = self.gLabel;
+        formatString = @"G: %1.2f";
+    }
+    
+    if (sender == self.bSlider) {
+        targetLabel = self.bLabel;
+        formatString = @"B: %1.2f";
+    }
+    
+    [targetLabel setText:[NSString stringWithFormat:formatString, ((UISlider*)sender).value]];
+    
+    _color = [UIColor colorWithRed:self.rSlider.value
+                             green:self.gSlider.value
+                              blue:self.bSlider.value
+                             alpha:1.0];
+    
+    self.navigationController.navigationBar.barTintColor = _color;
+    [self.controlView setBackgroundColor:_color];
+}
+
+- (IBAction)bgChangeValue:(id)sender {
+    UILabel *targetLabel;
+    NSString *formatString = @"%1.2f";
+    if (sender == self.rBgSlider) {
+        targetLabel = self.rBgLabel;
+        formatString = @"R: %1.2f";
+    }
+    
+    if (sender == self.gBgSlider) {
+        targetLabel = self.gBgLabel;
+        formatString = @"G: %1.2f";
+    }
+    
+    if (sender == self.bBgSlider) {
+        targetLabel = self.bBgLabel;
+        formatString = @"B: %1.2f";
+    }
+    
+    [targetLabel setText:[NSString stringWithFormat:formatString, ((UISlider*)sender).value]];
+    
+    UIColor *color = [UIColor colorWithRed:self.rBgSlider.value
+                                     green:self.gBgSlider.value
+                                      blue:self.bBgSlider.value
+                                     alpha:1.0];
+    [self.tableView setBackgroundColor:color];
+}
+
+- (IBAction)switchChanged:(id)sender {
+    if (sender == self.translucentSwitcher) {
+        NSLog(@"%@",self.translucentSwitcher.on?@"YES":@"NO");
+        [self.navigationController.navigationBar setTranslucent:self.translucentSwitcher.on];
+    }
+    
+    if (sender == self.blackNavBarSwitcher) {
+        NSLog(@"%@",self.blackNavBarSwitcher.on?@"YES":@"NO");
+        [self.navigationController.navigationBar setBarStyle:self.blackNavBarSwitcher.on?UIBarStyleBlack:UIBarStyleDefault];
+    }
+}
+
+
+@end
